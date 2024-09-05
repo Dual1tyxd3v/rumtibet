ad836a121b229ad2733bf20af935d68e633d9aaa
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-09-05 13:51:36 +0700

    Added popular responsive styles

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
 
+// burger
 burger.addEventListener('click', () => {
   burger.classList.toggle('header__burger--active');
   nav.classList.toggle('header__nav--active');
